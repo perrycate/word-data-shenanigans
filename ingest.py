d97149823927adfdfc07250bd16b5a5651bc0af5
Perry Cate <NPCate01@gmail.com>
2024-05-09 01:04:45 -0700

    print elapsed time

--- ingest.py
+++ ingest.py
@@ -65,5 +65,8 @@ print(f"Finished in {time.time() - start_time} seconds.")
 # and thus improve the performance. If you think the count for any single word in a doc
 # could be more than 65k, feel free to change - earlier iterations used a normal-size
 # int and worked fine.
+start_time = time.time()
+print("Massaging data into query-friendly format...")
 con.sql("CREATE TABLE words (word TEXT, count USMALLINT, date DATE, publisher TEXT, doc TEXT);")
 con.sql("INSERT INTO words (SELECT UNNEST(json_keys(count_by_word)) as word, (count_by_word->word)::USMALLINT, date, publisher, doc FROM raw_data);")
+print(f"Finished in {time.time() - start_time} seconds.")
